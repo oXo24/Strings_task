66d85461f9726847d444078568058def2780b802
pavly <pavlykas0124@gmail.com>
2023-07-02 17:05:41 +0300

    Add method for filtering exception and constructing the answer

--- src/task_2/Main.java
+++ src/task_2/Main.java
@@ -6,6 +6,8 @@ public class Main {
 
     private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
 
+    private static final String exception = "onion";
+
     private  static void main(String[] args) {
 
     }
@@ -13,6 +15,21 @@ public class Main {
     private static String[] providedData() {
         String[] data = {"orange", "plum", "tomato", "onion", "grape", "onion"};
         return data;
+    }
+
+    private static String processData(String[] data) {
+
+        StringBuilder sb = new StringBuilder();
+        int cnt = 1;
+        for (String name: data) {
+            if (!name.equalsIgnoreCase(exception)) {
+                sb.append(cnt++)
+                        .append(") ")
+                        .append(name)
+                        .append("\n");
+            }
+        }
+        return sb.toString();
     }
 
 
