ddbd30497b21a933b7ec91063a8c4d445f780f15
pavly <pavlykas0124@gmail.com>
2023-07-02 13:39:20 +0300

    Add provideData method for providing data

--- src/task_1/Main.java
+++ src/task_1/Main.java
@@ -1,4 +1,13 @@
 package task_1;
 
 public class Main {
+
+    public static void main(String[] args) {
+    }
+
+    private static String[] provideData() {
+        String[] data = {"brange", "plum", "tomato", "onibn", "grape"};
+        return data;
+    }
 }
+
