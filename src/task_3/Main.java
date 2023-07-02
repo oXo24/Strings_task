66911227b95644a208271f4e96eb614ad0dba06e
pavly <pavlykas0124@gmail.com>
2023-07-02 18:32:58 +0300

    Add logger for forming an answer and formed main

--- src/task_3/Main.java
+++ src/task_3/Main.java
@@ -1,4 +1,15 @@
 package task_3;
 
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+import java.util.logging.Logger;
+
 public class Main {
+    private static final Logger LOGGER = Logger.getLogger(task_2.Main.class.getName());
+
+    public static void main(String[] args) {
+        LOGGER.info("\n" + formAnswer());
+
+    }
+
 }
